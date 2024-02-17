b17ab5306c987d2a471f5c37a5f1845d6f6682db
Haimon Vieira <haimonvieira@pm.me>
2024-02-17 15:04:58 -0300

    Melhorias no modo escuro

--- script.js
+++ script.js
@@ -43,15 +43,30 @@ function abrirMenu(){
 function alternarModoEscuro() {
     let modoToggle = document.querySelector('.modo-toggle');
     let corpo = document.body;
+    let menuBar = document.querySelector('.menu-bar');
     corpo.classList.toggle('modo-escuro');
 
-    if(corpo.classList.contains("modo-escuro")){
-    modoToggle.style.backgroundColor = '#555';
-    }else{
-    modoToggle.style.backgroundColor = '#ddd';
-    }
+    localStorage.setItem('modoEscuro', corpo.classList.contains('modo-escuro'));
 
+    menuBar.style.backgroundColor = corpo.classList.contains('modo-escuro') ? 'rgb(104, 104, 104)' : '#fff';
+    modoToggle.style.backgroundColor = corpo.classList.contains('modo-escuro') ? '#555' : '#ddd';
 
     let bola = document.querySelector('.bola');
     bola.classList.toggle('bola-modo-escuro');
+
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    let corpo = document.body;
+    let bola = document.querySelector('.bola');
+    let modoToggle = document.querySelector('.modo-toggle');
+    let menuBar = document.querySelector('.menu-bar');
+    let modoEscuroSalvo = localStorage.getItem('modoEscuro') === 'true';
+
+    if (modoEscuroSalvo) {
+        corpo.classList.add('modo-escuro');
+        bola.classList.add('bola-modo-escuro');
+        modoToggle.style.backgroundColor = '#555';
+        menuBar.style.backgroundColor = 'rgb(104, 104, 104)';
+    }
+});
