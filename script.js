2aed1e49e0097497e2ed7865370b521eb8f48344
Haimon Vieira <haimonvieira@pm.me>
2024-02-16 22:07:50 -0300

    Adicionar menu bar

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+
+function abrirMenu(){
+    let cabecalho = document.querySelector('.cabecalho');
+    let cabecalhoMenu = document.querySelector('.cabecalho__menu');
+    let cabecalhoMenuDestaque = document.querySelector('.cabecalho__menu__destaque');
+    let menu = document.querySelector('.menu-inativo');
+    menu.classList.toggle('menu-ativo');
+    cabecalhoMenuDestaque.style.padding = '2%';
+    cabecalhoMenuDestaque.style.width = '100%';
+
+    cabecalho.classList.toggle('cabecalho-menu-ativo');
+
+    if(cabecalho.classList.contains("cabecalho-menu-ativo")){
+        cabecalhoMenu.style.display = 'flex';
+        cabecalhoMenu.style.flexDirection = 'column';
+    }else{
+        cabecalhoMenu.style.display = 'none'
+    }
+
+    //Verificar se o menu está aberto ou fechado e executar as devidas ações
+    if (cabecalho.classList.contains('cabecalho-menu-ativo')){
+        //Abrindo o Menu
+        setTimeout(() => {
+            cabecalho.style.alignItems = 'flex-start';
+            cabecalho.style.height = '100%';
+            cabecalho.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+            cabecalho.style.transition = '0.5s';
+        }, 1);
+        
+        //Adicionando o Evento de Fechar Menu ao Botão do Cabecalho
+    } else {
+        //Fechando o Menu
+        cabecalho.style.height = '';
+        cabecalho.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+
+    }
+}
